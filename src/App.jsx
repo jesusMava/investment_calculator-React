a2fb62a2b55a0fb4e716387d9da32390b876be01
user.email <jesus.macedo.michelada@kueski.com>
2024-07-11 06:14:05 -0600

    Inial components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/header'
+import UserInput from './components/userInput'
+
+function App() {
+  
+  return (
+    <>
+      <Header/>
+      <UserInput/>
+    </>
+  
+  )
+}
+
+export default App
