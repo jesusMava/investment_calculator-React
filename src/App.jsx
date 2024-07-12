d6cbe842b2e8a83ecb325bf072d1eb3649dc02f2
user.email <jesus.macedo.michelada@kueski.com>
2024-07-12 07:07:41 -0600

    Add table to show the investment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
+import { useState } from "react"
+
 import Header from './components/header'
-import UserInput from './components/userInput'
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+
 
 function App() {
-  
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  })
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(prevUserInput => {
+      return {
+        ...prevUserInput,
+        // el + sirve para convertirlo a valor numerico
+        [inputIdentifier]: +newValue
+      }
+    });
+  }
+
   return (
     <>
       <Header/>
-      <UserInput/>
+      <UserInput onChange={handleChange} userInput={userInput}/>
+      <Results inputs={userInput}/>
     </>
   
   )
